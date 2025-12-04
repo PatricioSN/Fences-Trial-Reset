25ed3ff5ca2e99953829aa30778fe00bfb84ec18
PatricioSN <patriciosppb@gmail.com>
2025-12-04 17:50:07 -0300

    Email_manage comments

--- Email_manage.py
+++ Email_manage.py
@@ -1,12 +1,15 @@
 from mailtm import Email
 import re
 import requests
+import threading
 
+#verify if the activation link is found to end the process
+done = threading.Event()
 
 def listener(message):
     raw = message.get('html') or message.get('text') or ''
 
-    #transform the message (currently is a list) to a string
+    #transforms the message (currently is a list) to a string
     content = raw if isinstance(raw, str) else str(raw)
 
     #list all links in the message
@@ -18,7 +21,9 @@ def listener(message):
             #access the link
             resp = requests.get(url, allow_redirects=True)
             print("Status: ", resp.status_code)
-            break
+            #enable closing the process
+            done.set()
+            return
 
 
 temp_email = Email()
@@ -26,7 +31,11 @@ temp_email = Email()
 temp_email.register()
 print("\nEmail Adress: " + str(temp_email.address))
 temp_email.start(listener)
+#flag to wait until the activation link is found
+done.wait()
+#close the email
 temp_email.stop()
+
 
 
 
