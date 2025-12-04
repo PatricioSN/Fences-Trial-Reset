8dffdf43c203a27973f06b728defd4a0f8f3ec24
PatricioSN <patriciosppb@gmail.com>
2025-12-04 18:28:09 -0300

    Email_manage to class

--- Email_manage.py
+++ Email_manage.py
@@ -1,40 +1,48 @@
+import time
 from mailtm import Email
 import re
 import requests
-import threading
 
-#verify if the activation link is found to end the process
-done = threading.Event()
+class Email_manage:
+    def __init__(self):
+        self.done = False
 
-def listener(message):
-    raw = message.get('html') or message.get('text') or ''
+    def listener(self, message):
+        raw = message.get('html') or message.get('text') or ''
 
-    #transforms the message (currently is a list) to a string
-    content = raw if isinstance(raw, str) else str(raw)
+        #transforms the message (currently is a list) to a string
+        content = raw if isinstance(raw, str) else str(raw)
 
-    #list all links in the message
-    urls = re.findall(r'(https?://[^\s"\']+)', content)
-    for url in urls:
-        #find the activation link
-        if "activate.api.stardock.net" in url or "validation/validate" in url:
-            print(f"Found activation link: {url}")
-            #access the link
-            resp = requests.get(url, allow_redirects=True)
-            print("Status: ", resp.status_code)
-            #enable closing the process
-            done.set()
-            return
+        #list all links in the message
+        urls = re.findall(r'(https?://[^\s"\']+)', content)
 
+        for url in urls:
 
-temp_email = Email()
-#create a new random email
-temp_email.register()
-print("\nEmail Adress: " + str(temp_email.address))
-temp_email.start(listener)
-#flag to wait until the activation link is found
-done.wait()
-#close the email
-temp_email.stop()
+            #find the activation link
+            if "activate.api.stardock.net" in url or "validation/validate" in url:
+                print(f"Found activation link: {url}")
+
+                #access the link
+                resp = requests.get(url, allow_redirects=True)
+                print("Status: ", resp.status_code)
+
+                #enable closing the process
+                self.done = True
+                return
+
+    def run(self):
+        temp_email = Email()
+        temp_email.register() #create a new random email
+        print("\nEmail Adress: " + str(temp_email.address))
+
+        temp_email.start(self.listener)
+
+        #flag to wait until the activation link is found
+        while not self.done:
+            time.sleep(0.1)
+
+        #close the email
+        temp_email.stop()
 
 
 
--- Fences_manage.py
+++ Fences_manage.py
@@ -5,22 +5,33 @@ import time
 pyautogui.PAUSE = 0.5
 
 #Open fences
-pyautogui.press('winleft')
-pyautogui.write('fences')
-pyautogui.press('enter')
+def open_fences():
+    pyautogui.press('winleft')
+    pyautogui.write('fences')
+    pyautogui.press('enter')
+open_fences()
 
 #click on 'start trial'
 time.sleep(1)
-position = pyautogui.locateCenterOnScreen(r"Assets\Start_30_Day_Trial.png")
-pyautogui.click(position)
+def start_trial():
+    try:
+        position = pyautogui.locateCenterOnScreen(r"Assets\Start_30_Day_Trial.png")
+    #Tries two times, because the fences have a problem sometimes. If you immediately delete the cache, maybe the program doesn't open the screen of the trial test
+    except pyautogui.ImageNotFoundException:
+        pyautogui.hotkey('alt','f4')
+        time.sleep(0.5)
+        open_fences()
+        position = pyautogui.locateCenterOnScreen(r"Assets\Start_30_Day_Trial.png")
+    pyautogui.click(position)
+start_trial()
 
-
-#TODO: 3- criar um email temporario
+#criar um email temporario
+#(preciso adaptar a classe para copiar o email, informar ao fences, e só depois ela prosseguir)
 
 #TODO: 4- informar esse email temporario para o fences
 
 #TODO: 5- clica em proximo
 
-#TODO: 6- verificar a caixa de mensagens do email
+#verificar a caixa de mensagens do email
 
-#TODO: 7- clicar no link dentro do email recebido
+#clicar no link dentro do email recebido
